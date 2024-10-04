a8b50a7c4c06de79d3236f98aacdecf0dc8a11ea
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-04 17:09:59 +0200

    User and account changes

--- RVAProdavnica.Data/User.cs
+++ RVAProdavnica.Data/User.cs
@@ -21,7 +21,6 @@ namespace RVAProdavnica.Data
         public string? Email { get; set; }
         [Column("password")]
         public string Password { get; set; }
-
         public enum Role { ADMIN, PREMIUM_USER, USER }
     }
 
--- RVAProdavnica.Web/wwwroot/js/account.js
+++ RVAProdavnica.Web/wwwroot/js/account.js
@@ -1,5 +1,3 @@
-
-
 // const x = document.getElemenyById('accountId').value
 // document.getElementById("accountId").onclick = function () {
 //     window.location.href = 'http://localhost:7210/Home/Index'
