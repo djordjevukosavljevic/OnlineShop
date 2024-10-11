618f567712c1b0d4bfb862019ecb48beafeedf79
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-11 18:57:15 +0200

    Created Images Set in DataContext

--- RVAProdavnica.Data/DataContext.cs
+++ RVAProdavnica.Data/DataContext.cs
@@ -16,17 +16,19 @@ namespace RVAProdavnica.Data
 
         }
 
-        public DbSet<User> Users { get; set; }
         public DbSet<Product> Products { get; set; }
-        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Image> Images { get; set; }
         public DbSet<Car> Cars { get; set; }
 
         public DbSet<BugReport> BugReports { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
             modelBuilder.Entity<Product>();
+            modelBuilder.Entity<Image>();
             modelBuilder.Entity<User>();
             modelBuilder.Entity<Car>();
             modelBuilder.Entity<BugReport>();
