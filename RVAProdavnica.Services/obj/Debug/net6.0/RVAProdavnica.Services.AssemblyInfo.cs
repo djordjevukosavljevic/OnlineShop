02a8d4ed03c5eb3db7f82400b6cd8837d2120257
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-11-04 18:55:28 +0100

    UserController updates

--- RVAProdavnica.Services/obj/Debug/net6.0/RVAProdavnica.Services.AssemblyInfo.cs
+++ RVAProdavnica.Services/obj/Debug/net6.0/RVAProdavnica.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RVAProdavnica.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a26bec5bfa0a5f6085b8ce7138b5f7d4325a5d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0c3151ea373162047c23a1a1bcb980ea18d248")]
 [assembly: System.Reflection.AssemblyProductAttribute("RVAProdavnica.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RVAProdavnica.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
