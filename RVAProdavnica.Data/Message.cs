245bd10bdc584d75938486c87f1881dad88da1ce
root <root@zorzezorz.zorzezorz>
2024-12-24 11:15:18 +0100

    message entity deleted comment

--- RVAProdavnica.Data/Message.cs
+++ RVAProdavnica.Data/Message.cs
@@ -1,15 +1,15 @@
-// using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 
-// namespace RVAProdavnica.Data
-// {
-//     [Table("message")]
-//     public class Message
-//     {   
-//         [Column("user")]
-//         public User User;
-//         [Column("size")]
-//         public double Size;
-//         [Column("text")]
-//         public String Text;
-//     }
-// }
+namespace RVAProdavnica.Data
+{
+    [Table("message")]
+    public class Message
+    {   
+        [Column("user")]
+        public User User;
+        [Column("size")]
+        public double Size;
+        [Column("text")]
+        public String Text;
+    }
+}
