c53cafa3c8e7f993abda570fe65abb72ce962544
root <root@zorzezorz.zorzezorz>
2025-01-05 18:14:22 +0100

    yeah no...

--- RVAProdavnica.Services/obj/Debug/net6.0/RVAProdavnica.Services.AssemblyInfo.cs
+++ RVAProdavnica.Services/obj/Debug/net6.0/RVAProdavnica.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RVAProdavnica.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+245bd10bdc584d75938486c87f1881dad88da1ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb185c529bc1648a54eb62c90f3e96da32ebae3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RVAProdavnica.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RVAProdavnica.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
