6e60e7cbef7a5ff54b8fbfdf8e3685d1d0844472
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-02-18 22:13:56 +0100

    stylezz

--- RVAProdavnica.Web/obj/Debug/net6.0/RVAProdavnica.Web.AssemblyInfo.cs
+++ RVAProdavnica.Web/obj/Debug/net6.0/RVAProdavnica.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RVAProdavnica.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d6bc7742b78628a23104716347f28d6b66e852c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bfff9914c269c3da7c395eebd446ce5cae8d688")]
 [assembly: System.Reflection.AssemblyProductAttribute("RVAProdavnica.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RVAProdavnica.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
