9604f782854940cce152584b3f7c88ce1a300bc7
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-02-27 14:23:44 +0100

    small updates

--- RVAProdavnica.Web/wwwroot/js/ajaxCalls.js
+++ RVAProdavnica.Web/wwwroot/js/ajaxCalls.js
@@ -4,24 +4,23 @@
         url: url,
         data: data,
         success: function (response) {
-            console.log("(|)");
             var len = $(response).find('.counter-rows').length;
             if (len != null && len > 0) {
                 dataObject.append(response);
                 $(".row-helper").fadeIn();
             }
             if (pageNumber > 1) {
-                $("#load-older").html("Load older...");
+                $("#load-older").html("Load older");
                 $("#load-older").prop('disabled', false);
             } else {
-                $("#load-older").html("No older data...");
+                $("#load-older").html("No older data");
                 $("#load-older").prop('disabled', true);
             }
             if (len < 5) {
-                $("#load-more").html("No more data...");
+                $("#load-more").html("No more data");
                 $("#load-more").prop('disabled', true);
             } else {
-                $("#load-more").html("Load more...");
+                $("#load-more").html("Load more");
                 $("#load-more").prop('disabled', false);
             }
         },
