8208e72c24fa9311f7b7186bb647f79279ff97fb
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-03-04 18:42:07 +0100

    Wierd changes

--- RVAProdavnica.Data/obj/Debug/net6.0/RVAProdavnica.Data.AssemblyInfo.cs
+++ RVAProdavnica.Data/obj/Debug/net6.0/RVAProdavnica.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RVAProdavnica.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75ad2640fd944344505d4612de1a5794504075f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c8d5dd253d7409d38986a152ec2d18182f3897")]
 [assembly: System.Reflection.AssemblyProductAttribute("RVAProdavnica.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RVAProdavnica.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
