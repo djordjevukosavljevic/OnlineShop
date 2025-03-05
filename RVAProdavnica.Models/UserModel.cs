3a01efde5453e2a61375677483cbb719246ac726
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-03-06 00:16:42 +0100

    model small updates

--- RVAProdavnica.Models/UserModel.cs
+++ RVAProdavnica.Models/UserModel.cs
@@ -6,16 +6,27 @@ namespace RVAProdavnica.Models
     public class UserModel
     {
         
-        [Column("id")]
+     
         [Key]
         public int Id { get; set; }
-      
-        [Column("username")]
-        public string Username { get; set; }
-     
-        [Column("password")]
-        public string Password { get; set; }
         
+        public DateTime DateCreatedAt { get; set; }
+
+        public DateTime DateUpdatedAt { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Username {get;set;}
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public Role role {get;set;}
+
+        public enum Role { ADMIN, PREMIUM_USER, USER }
         
     }
 
