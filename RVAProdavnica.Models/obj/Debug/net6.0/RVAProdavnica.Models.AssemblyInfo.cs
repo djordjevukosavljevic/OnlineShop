a7a2289675f0eabb049a2f74e62c9c906e0da144
Djordje Vukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-04-17 15:43:44 +0200

    login index view

--- RVAProdavnica.Models/obj/Debug/net6.0/RVAProdavnica.Models.AssemblyInfo.cs
+++ RVAProdavnica.Models/obj/Debug/net6.0/RVAProdavnica.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RVAProdavnica.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722a81e9cd6fc4fcf0c32e8dcf3122776a682aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a024fdd9c7d861908f58a787037a18754cf68a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RVAProdavnica.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RVAProdavnica.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RVAProdavnica.Services/obj/Debug/net6.0/RVAProdavnica.Services.AssemblyInfo.cs
+++ RVAProdavnica.Services/obj/Debug/net6.0/RVAProdavnica.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RVAProdavnica.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722a81e9cd6fc4fcf0c32e8dcf3122776a682aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a024fdd9c7d861908f58a787037a18754cf68a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RVAProdavnica.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RVAProdavnica.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
