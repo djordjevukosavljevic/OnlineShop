72b00ce66c752299f64120901d2af8638798b00e
Djordje Vukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-05-15 13:10:57 +0200

    Small changes

--- RVAProdavnica.Models/obj/Debug/net6.0/RVAProdavnica.Models.AssemblyInfo.cs
+++ RVAProdavnica.Models/obj/Debug/net6.0/RVAProdavnica.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RVAProdavnica.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02076f8b6f6ce3d402223c7025c750b23eca05b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+264de9826b132451d33c8d8590e6ac7b67d4504a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RVAProdavnica.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RVAProdavnica.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
