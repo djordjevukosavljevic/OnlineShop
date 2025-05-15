1fd26eccd1b9016db54d676a7826d6a768d18781
Djordje Vukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-05-15 13:17:46 +0200

    MessageModel

--- RVAProdavnica.Models/MessageModel.cs
+++ RVAProdavnica.Models/MessageModel.cs
@@ -1,7 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RVAProdavnica.Models
 {
     public class MessageModel
-    {
+    {   
+        [Key]
+        public int Id { get; set; }
 
+        public DateTime DateCreatedAt { get; set; }
+        
+        public DateTime DateUpdatedAt { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Text { get; set; }
     }
 }
